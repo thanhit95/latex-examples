3b6db29b2084ea1ae72bb61734a0d64823756cb8
thanhit95 <thanh.it1995@gmail.com>
2023-05-07 12:46:42 +0700

    add build and cleanup script

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+
+TOOL_EXEC=latexmk
+TMP_DIR_NAME=ztmp_latex
+TEX_FILE_PATH=$1
+
+
+if [[ -z "${TEX_FILE_PATH}" ]]; then
+    echo "Missing 1st argument: TEX_FILE_PATH"
+    exit 1
+fi
+if [[ ! -f "${TEX_FILE_PATH}" ]]; then
+    echo "${TEX_FILE_PATH} is not a file"
+    exit 1
+fi
+if [[ "${TEX_FILE_PATH}" != *.tex ]]; then
+    echo "${TEX_FILE_PATH} is not a tex file"
+    exit 1
+fi
+
+
+TEX_FILE_PATH="$(readlink -f "${TEX_FILE_PATH}")"
+TEX_DIR_PATH="$(dirname -- "${TEX_FILE_PATH}")"
+TEX_FILE_NAME_NOEXT=$(basename -- "$TEX_FILE_PATH")
+TEX_FILE_NAME_NOEXT="${TEX_FILE_NAME_NOEXT%.*}"
+
+TMP_DIR_PATH="${TEX_DIR_PATH}/${TMP_DIR_NAME}"
+TMP_PDF_PATH="${TMP_DIR_PATH}/${TEX_FILE_NAME_NOEXT}.pdf"
+
+OUT_PDF_PATH="${TEX_DIR_PATH}/${TEX_FILE_NAME_NOEXT}.pdf"
+
+
+echo "TEX_DIR_PATH: ${TEX_DIR_PATH}"
+echo "TEX_FILE_NAME_NOEXT: ${TEX_FILE_NAME_NOEXT}"
+echo "TMP_PDF_PATH: ${TMP_PDF_PATH}"
+echo "OUT_PDF_PATH: ${OUT_PDF_PATH}"
+echo
+echo "Press Enter to progress"
+read -s -n 1 key
+
+
+"${TOOL_EXEC}" -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir="${TMP_DIR_PATH}" "${TEX_FILE_PATH}"
+
+
+if [[ -f "${TMP_PDF_PATH}" ]]; then
+    mv "${TMP_PDF_PATH}" "${OUT_PDF_PATH}"
+fi
---
+++ cleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+TEX_DIR_PATH="$(dirname "$(readlink -f "$0")")"
+
+TMP_DIR_NAME=ztmp_latex
+TMP_DIR_PATH="${TEX_DIR_PATH}/${TMP_DIR_NAME}"
+
+TMP_DIR_NAME2=svg-inkscape
+TMP_DIR_PATH2="${TEX_DIR_PATH}/${TMP_DIR_NAME2}"
+
+
+echo "TEX_DIR_PATH: ${TEX_DIR_PATH}"
+echo
+echo "pdf files to be removed:"
+find "${TEX_DIR_PATH}" -maxdepth 1 -mindepth 1 -type f -name "*.pdf" 2>/dev/null
+echo
+[[ -d "${TMP_DIR_PATH}" ]] && echo "directory to be removed: ${TMP_DIR_PATH}"
+[[ -d "${TMP_DIR_PATH2}" ]] && echo "directory to be removed: ${TMP_DIR_PATH2}"
+echo
+echo "Press Enter to progress"
+read -s -n 1 key
+
+
+[[ -d "${TMP_DIR_PATH}" ]] && rm -rf "${TMP_DIR_PATH}"
+[[ -d "${TMP_DIR_PATH2}" ]] && rm -rf "${TMP_DIR_PATH2}"
+# rm -f "${TEX_DIR_PATH}"/*.pdf
+find "${TEX_DIR_PATH}" -maxdepth 1 -mindepth 1 -type f -name "*.pdf" -exec rm -f '{}' \; 2>/dev/null
